574d54239314bf7cc7f30a057ca669d2f1592b0c
Diego Fernandes <diego.schell.f@gmail.com>
2024-10-10 10:50:10 -0300

    chore: update db seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,28 +1,47 @@
-import { client, db } from '.'
-import { goalCompletions, goals } from './schema'
+import { makeGoal } from '@/test/factories/make-goal'
+import { makeGoalCompletion } from '@/test/factories/make-goal-completion'
+import { makeUser } from '@/test/factories/make-user'
 import dayjs from 'dayjs'
+import { client, db } from '.'
+import { goalCompletions, goals, users } from './schema'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
+  await db.delete(users)
 
-  const result = await db
-    .insert(goals)
-    .values([
-      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
-      { title: 'Me exercitar', desiredWeeklyFrequency: 3 },
-      { title: 'Meditar', desiredWeeklyFrequency: 1 },
-    ])
-    .returning()
+  const user = await makeUser()
+
+  const [goal1, goal2] = await Promise.all([
+    makeGoal({
+      userId: user.id,
+      title: 'Acordar cedo',
+      desiredWeeklyFrequency: 5,
+    }),
+    makeGoal({
+      userId: user.id,
+      title: 'Me exercitar',
+      desiredWeeklyFrequency: 3,
+    }),
+    makeGoal({ userId: user.id, title: 'Meditar', desiredWeeklyFrequency: 1 }),
+  ])
 
   const startOfWeek = dayjs().startOf('week')
 
-  await db.insert(goalCompletions).values([
-    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
-    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
-  ])
+  await makeGoalCompletion({
+    goalId: goal1.id,
+    createdAt: startOfWeek.toDate(),
+  })
+  await makeGoalCompletion({
+    goalId: goal2.id,
+    createdAt: startOfWeek.add(1, 'day').toDate(),
+  })
 }
 
-seed().finally(() => {
-  client.end()
-})
+seed()
+  .then(() => {
+    console.log('Database seeded!')
+  })
+  .finally(() => {
+    client.end()
+  })
