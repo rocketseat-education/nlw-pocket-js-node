3bb9efd8a83df1f25cf1e4e13af2e92ee8682b37
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-10-11 13:18:11 -0300

    Criando App OAuth no GitHub

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,9 @@ import z from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
