a591908ccc333b80426c27a1bcb705ce2689400d
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-10-17 15:46:15 -0300

    Calculando nível do usuário

---
+++ src/modules/gamification.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from 'vitest'
+import {
+  calculateExperienceToLevel,
+  calculateLevelFromExperience,
+} from './gamification'
+
+test('experience to level', () => {
+  const exp1 = calculateExperienceToLevel(1)
+  const exp2 = calculateExperienceToLevel(2)
+  const exp4 = calculateExperienceToLevel(4)
+
+  expect(exp1).toEqual(0)
+  expect(exp2).toEqual(26)
+  expect(exp4).toEqual(43)
+})
+
+test('level from experience', () => {
+  const lev1 = calculateLevelFromExperience(10)
+  const lev2 = calculateLevelFromExperience(27)
+  const lev5 = calculateLevelFromExperience(26 + 33 + 43)
+
+  expect(lev1).toEqual(1)
+  expect(lev2).toEqual(2)
+  expect(lev5).toEqual(4)
+})
---
+++ src/modules/gamification.ts
@@ -0,0 +1,17 @@
+const BASE_EXPERIENCE = 20
+const EXPERIENCE_FACTOR = 1.3
+
+export function calculateLevelFromExperience(experience: number) {
+  return (
+    Math.floor(
+      Math.log((experience / BASE_EXPERIENCE) * (EXPERIENCE_FACTOR - 1) + 1) /
+        Math.log(EXPERIENCE_FACTOR)
+    ) + 1
+  )
+}
+
+export function calculateExperienceToLevel(level: number) {
+  if (level === 1) return 0 // Nível 1 começa com 0 XP adicional
+
+  return Math.floor(BASE_EXPERIENCE * EXPERIENCE_FACTOR ** (level - 1))
+}
